bcd0f44fb409be69dd328075895676e0a47b8952
SundayYogurt <156178493+SundayYogurt@users.noreply.github.com>
2024-01-20 17:06:51 +0700

    update

--- updateDd.php
+++ updateDd.php
@@ -1,5 +1,5 @@
 <?php
-echo "CustomerID = " . $_GET['CustomerID'];
+// echo "CustomerID = " . $_GET['CustomerID'];
 $CIDs = $_GET['CustomerID'];
 require "connect.php";
 $sql_2 = "select * from country";
@@ -10,14 +10,18 @@ $stmt_2->execute();
 <html>
 
 <head>
-    <title>User Registration11</title>
+    <title>User Registration</title>
     <script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
-
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
 </head>
 
-<body>
 
-    <h1>Update Customer with Dropdown Menu</h1>
+
+<body>
+    <div class="col d-flex justify-content-center">
+        <h1>Update Customer</h1>
+    </div>
+    <br>
     <?php
     require 'connect.php';
     $sql_select =  "SELECT * FROM country ORDER BY CountryCode";
@@ -32,37 +36,62 @@ $stmt_2->execute();
     $stmt = $conn->prepare($sql_select_customer);
     $stmt->bindparam(':CID', $CIDs);
     $stmt->execute();
-    echo "get = " . $_GET['CustomerID'];
+    // echo "get = " . $_GET['CustomerID'];
     $result = $stmt->fetch(PDO::FETCH_ASSOC);
-    echo "1234" . $result['CustomerID'] . $result['Name'];
+    // echo "1234" . $result['CustomerID'] . $result['Name'];
     //} 
-    echo "3314"  ?>
+    // echo "3314"  
+    ?>
 
-    <form action="update_real.php" method="POST">
-        <input type="text" placeholder="Enter Customer ID" name="CustomerID" required value=<?= $result['CustomerID']; ?>>
-        <br> <br>
-        <input type="text" placeholder="Name" name="Name" required value=<?= $result['Name'] ?>>
-        <br> <br>
-        <input type="number" placeholder="OutStandingDebt" name="outstandingDebt" value=<?= $result['OutstandingDebt'] ?>>
-        <br> <br>
-        <input type="email" placeholder="Email" name="email" value=<?= $result['Email'] ?>>
-        <br> <br>
-        <input type="date" placeholder="Birthdate" name="birthdate" value=<?= $result['Birthdate'] ?>>
-        <br> <br>
-        <label> กรุณาใส่รหัสประเทศ </label>
-        <select name="countrycode">
-            <?php while ($cc = $stmt_2->fetch(PDO::FETCH_ASSOC)) :
-            ?>
-                <option value="<?php echo $cc['CountryCode'] ?>">
-                    <?php echo $cc['CountryName'] ?>
-                </option>
-            <?php
-            endwhile;
-            ?>
-        </select>
-        <br> <br>
-        <input type="submit" placeholder="submit">
-    </form>
-</body>
+
+
+    <div class="col d-flex justify-content-center">
+        <div class="form-outline mb-4">
+            <div class="row mb-4">
+                <div class="border" style="width: 400px">
+                    <form action=" update_real.php" method="POST">
+                        <div class="table table-striped table-hover table-responsive table-bordered">
+                            <label class="form-label">CustomerID</label>
+                            <br>
+                        </div>
+                        <input type="text" for="form1Example2" class="form-control" placeholder="Enter Customer ID" name="CustomerID" width="10px" value=<?= $result['CustomerID']; ?>>
+                        <br>
+                        <label class="form-label">Name</label>
+                        <input type="text" for="form1Example2" class="form-control" placeholder="Name" name="Name" required value=<?= $result['Name'] ?>>
+                        <br>
+                        <label class="form-label">OutstandingDebt</label>
+                        <input type="number" for="form1Example2" class="form-control" placeholder="OutStandingDebt" name="outstandingDebt" value=<?= $result['OutstandingDebt'] ?>>
+                        <br>
+                        <label class="form-label">Email</label>
+                        <input type="email" for="form1Example2" class="form-control" placeholder="Email" name="email" value=<?= $result['Email'] ?>>
+                        <br>
+                        <label class="form-label">BirthDate</label>
+                        <input type="date" for="form1Example2" class="form-control" placeholder="Birthdate" name="birthdate" value=<?= $result['Birthdate'] ?>>
+                        <br>
+                        <label> กรุณาใส่รหัสประเทศ </label>
+                        <select name="countrycode">
+
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <?php while ($cc = $stmt_2->fetch(PDO::FETCH_ASSOC)) :
+    ?>
+        <option value="<?php echo $cc['CountryCode'] ?>">
+            <?php echo $cc['CountryName'] ?>
+        </option>
+    <?php
+    endwhile;
+    ?>
+    </select>
+    <br> <br>
+
+    <div class="col d-flex justify-content-center">
+
+        <button data-mdb-ripple-init type="submit" class="btn btn-primary btn-block" style="width:60px"> ok </button>
+        </form>
+    </div>
+    </ </body>
 
 </html>
--- update_real.php
+++ update_real.php
@@ -1,42 +1,54 @@
 <script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+<script src="https://code.jquery.com/jquery-3.7.1.js"></script>
 <?php
+
 try {
+  if (isset($_POST['CustomerID']) && isset($_POST['Name'])) {
+    require 'connect.php';
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-    // echo "<br>" . $_POST['customerID'] . $_POST['Name'] . $_POST['birthdate'] . $_POST['email'] .
-    //     $_POST['countryCode'] . $_POST['outstandingDebt'];
+    $sql = "UPDATE customer SET name = :name, birthdate = :birthdate, email = :email,
+            countryCode = :countryCode, outstandingDebt = :outstandingDebt 
+            WHERE customerID = :customerID";
 
-    if (isset($_POST['CustomerID']) && isset($_POST['Name'])) :
+    $stmt = $conn->prepare($sql);
+    $stmt->bindParam(':customerID', $_POST['CustomerID']);
+    $stmt->bindParam(':name', $_POST['Name']);
+    $stmt->bindParam(':birthdate', $_POST['birthdate']);
+    $stmt->bindParam(':email', $_POST['email']);
+    $stmt->bindParam(':countryCode', $_POST['countrycode']);
+    $stmt->bindParam(':outstandingDebt', $_POST['outstandingDebt']);
 
-        require 'connect.php';
-        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        $sql = "UPDATE customer SET name = :name, birthdate = :birthdate, email = :email,
-    countryCode = :countryCode, outstandingDebt = :outstandingDebt WHERE customerID = :customerID"; //llll
+    $stmt->execute(); // Execute the update query
 
-        $stmt = $conn->prepare($sql);
-        $stmt->bindParam(':customerID', $_POST['CustomerID']);
-        $stmt->bindParam(':name', $_POST['Name']);
-        $stmt->bindParam(':birthdate', $_POST['birthdate']);
-        $stmt->bindParam(':email', $_POST['email']);
-        $stmt->bindParam(':countryCode', $_POST['countrycode']);
-        $stmt->bindParam(':outstandingDebt', $_POST['outstandingDebt']);
-        if ($stmt->execute()) :
-            header("location:index.php");
-        else :
-            $message = 'Fail to update customer';
-        endif;
-        echo $message;
-        echo '<script>
-      Swal.fire({
-        title: "UPDATED",
-        text: "อัพเดตแล้ว",
-        icon: "success",
-        confirmButtonText: "ยอดเยี่ยมเลย",
-      }); 
-     </script>';
-        $conn = null;
-
-    endif;
+    if ($stmt->rowCount() > 0) {
+      echo '<script type="text/javascript">
+            $(document).ready(function(){
+              Swal.fire({
+                title: "UPDATED",
+                text: "อัพเดตแล้ว",
+                icon: "success",
+                confirmButtonText: "ยอดเยี่ยมเลย",
+              }).then(function(){
+                window.location.href = "index.php";
+              });
+            });
+          </script>';
+    } else {
+      echo '<script type="text/javascript">
+            $(document).ready(function(){
+              Swal.fire({
+                title: "แย่จริง",
+                text: "ไม่สามารถอัพเดตได้",
+                icon: "error",
+                confirmButtonText: "ok",
+              }).then(function(){
+                window.location.href = "index.php";
+              });
+            });
+          </script>';
+    }
+  }
 } catch (PDOException $e) {
-    echo $e->getMessage();
+  echo $e->getMessage();
 }
-?>
